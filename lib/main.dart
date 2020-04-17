343fd5e465deccda7be617cb584e135cca63cd4d
adrijshikhar <adrijshikhar85@gmail.com>
2020-04-17 20:21:46 +0530

    Adds body

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(appBar: AppBar(title: Text("My File App"))),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My File App"),
+        ),
+        body: Text("This is my default text!"),
+      ),
     );
   }
 }
