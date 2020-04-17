707cfaacfebe82ca2390e2bc3dcf0c5a4a547f07
adrijshikhar <adrijshikhar85@gmail.com>
2020-04-17 20:19:32 +0530

    Adds AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello'),
+      home: Scaffold(appBar: AppBar(title: Text("My File App"))),
     );
   }
 }
