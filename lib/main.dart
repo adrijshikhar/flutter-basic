f67e4c2cdd289714011c7a6dfc9c662f53caaa06
adrijshikhar <adrijshikhar85@gmail.com>
2020-04-20 11:54:02 +0530

    Adds button

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,40 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+void answerQuestion() {
+  print('Answer chosen');
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    List<String> questions = [
+      "What is your fav color?",
+      "What is your fav animal?"
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("My File App"),
         ),
-        body: Text("This is my default text!"),
+        body: Column(
+          children: [
+            Text("The question"),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
       ),
     );
   }
